7603366c04835ab3453318246527e5e60fc2bd32
MilosMiki <miki2000milos@gmail.com>
2025-11-17 21:30:22 +0100

    trying to fix vite browsee integration

--- gpgsl-two-step-boost/src/main.jsx
+++ gpgsl-two-step-boost/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import browsee from "@browsee/web-sdk";
 
-createRoot(document.getElementById('root')).render(
+browsee.init({ apiKey: "85fdc0534d8b99deb4d546170614f5c754a023663dcdb665" });
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
